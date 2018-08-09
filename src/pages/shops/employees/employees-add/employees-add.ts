fbdf3acc49b366b873767ab4282cacfb343ea51a
qinjunbang <745068358@qq.com>
2018-08-09 14:08:35 +0800

    员工添加页面修改

--- src/pages/shops/employees/employees-add/employees-add.ts
+++ src/pages/shops/employees/employees-add/employees-add.ts
@@ -5,17 +5,13 @@ import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { HttpService } from '../../../../providers/HttpService';
 import { Observable } from 'rxjs/Rx';
-// import  cityData    from '../../../assets/chinese-cities.json';
+
 
 @Component({
   selector: 'page-employees-add',
   templateUrl: 'employees-add.html'
 })
 export class EmployeesAddPage {
-
-   public cities = []; // 城市JSON数据
-   public startTime:string = '08:00';
-   public endTime:string = '22:00';
 
   constructor(
     public http: HttpService,
@@ -25,26 +21,8 @@ export class EmployeesAddPage {
   }
   // 页面初始化完成
   ionViewDidLoad() {
-    // 初始化省份城市区域
-    this.getCitiesData().subscribe(data => {
-      console.log("data", data.json());
-      this.cities = data.json();
 
-    });
   }
 
-  // 获得省份城市区域数据
-  getCitiesData() {
-    return Observable.create(observer => {
-      this.httpService.get('assets/chinese-cities.json').subscribe(data => {
-        observer.next(data);
-      });
-    });
-  }
-
-  // 选择省份城市区域回调
-  changeCitiesDate(event) {
-    console.log("6666", event);
-  }
 
 }
--- src/providers/Config.ts
+++ src/providers/Config.ts
@@ -2,8 +2,9 @@
  * Created by HIAPAD on 2018/7/26.
  */
 export class Config {
-  // public static app_serve_url = 'https://r.zhanglitong.com';
-  public static app_serve_url = 'http://localhost:8100/';
+  // public static app_serve_url = 'https://r.zhanglitong.com'; // 打包
+  public static app_serve_url = 'http://localhost:8100/';  // web浏览器调试
+  // public static app_serve_url = 'http://192.168.0.144:8100/'; // 真机调试
   public static token = ''; // token
   public static userInfo = []; // user info
 }
